13d5f8ce49c3db132ede80f3e5c727c1f44982ad
Carlos Blanco <carlojbm04@gmail.com>
2024-05-30 09:56:25 +0200

    Two sum | LeetCode in C#

--- codeforces/two_pointers/C.cpp
+++ codeforces/two_pointers/C.cpp
@@ -1,11 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(0);
+#define int long long
 
-    int n, m;
-    cin >> n >> m;
+main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m; cin >> n >> m;
 
     vector<int> a(n);
     for(int i=0;i<n;i++) cin >> a[i];
@@ -13,29 +15,38 @@ int main() {
     vector<int> b(m);
     for(int i=0;i<m;i++) cin >> b[i];
 
-    int i = 0, j = 0, counter = 0;
+    vector<int> vswap;
 
-    vector<int> c;
-
-    while(i < a.size() || j < b.size()) {
-        if(j == b.size() || i < a.size() && a[i] <= b[j]) {
-            c.push_back(a[i++]);
-        }
-        else {
-            c.push_back(b[j++]);
-        }
+    if(n < m) {
+        vswap = a;
+        a = b;
+        b = vswap;
     }
 
-    for(int i=0;i<c.size();i++) {
-        int j = i+1;
-        cout << c[i] << ' ';
-        if(binary_search(a.begin(), a.end(), c[i]) && binary_search(b.begin(), b.end(), c[i]))
-            while(c[i] == c[j++])
-                ++counter;
+    int j = 0;
+    map<int, int> counter, fr; // value -> frecuency
+
+    for(int i=0;i<b.size();i++) {
+        counter[b[i]]++;
     }
 
-    cout << endl;
+    for(int i=0;i<a.size();i++) {
+        fr[a[i]]++;
+    }
 
-    cout << counter;
+    long long res = 0;
+
+    for(auto x: counter) {
+        //cout << x.first << " " << fr[x.first] << " " << x.second << endl;
+        res += (fr[x.first] * x.second);
+    }
+
+    cout << res;
 
 }
+
+/*
+8 7
+1 1 3 3 3 5 8 8
+1 3 3 4 5 5 5
+*/
